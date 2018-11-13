f7ebe85c7f3e48e24407bf51115ec7fdcc89126d
Stuart Adair <stu@silverka.co.uk>
2018-11-13 15:20:54 +0000

    Changed form method to post

--- app.rb
+++ app.rb
@@ -13,8 +13,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/give-cat-name' do
+  erb(:form)
 end
